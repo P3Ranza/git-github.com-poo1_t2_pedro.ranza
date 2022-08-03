50aa9e3029eec34404f468ba64258a25abe20797
travencolo <travencolo@gmail.com>
2022-08-03 18:53:56 -0300

    Correcao Prova 1

---
+++ provas/p1/Prova 1 POO/Class.java
@@ -0,0 +1,59 @@
+
+import java.util.Scanner;
+import static java.lang.Math.PI;
+
+
+public class ponto (String args[]) {// check:<<<erro: não é dessa forma que se cria a classe ponto>>>>
+
+
+    System.out.println("Escreva o valor do de cada coordenada x e y");
+    float x, y;
+    Scanner pr = new Scanner(System.in);
+
+    for(int i > 0; i <= 10; ++i){
+        for (int j > 0; j <= 10; ++j){
+            double cordenada[ i][j];
+            x = pr.nextDouble();
+            y = pr.nextDouble();
+        return j = j + 1;
+        }
+    return i = i + 1;
+    }
+    
+
+    return cordenada[][];
+
+}
+
+// check:<<<erro: classe círculo definida de forma bem errada>>>>
+public class circulo (float pi=3,14) {
+
+    public double area() {
+
+        return pi * r * r;
+
+    }
+
+    public static float peri() {// check:<<<erro: e o parâmetro?>>>>
+        return 2 * pi * r;
+
+    }
+}
+
+
+// check:<<<erro: estão faltando muitos conceitos de POO - vou ter que zerar a prova>>>>
+public static void main (Strings args[]){
+
+    ponto p1[][] = new ponto();
+
+    float r;
+    public float getR() {
+        return r;
+    }
+
+    double r r1 = new r.getR()
+
+    r.area();
+    r.peri();
+
+}
